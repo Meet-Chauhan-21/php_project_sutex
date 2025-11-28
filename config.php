ddb4ec0ebb8c68d9cc0e0233aec30c4fed7af70a
meet chauhan <meetchauhan9915@gmail.com>
2025-11-28 16:31:01 +0530

    port change

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = '';
 $DB_NAME = 'vidhyaguru_db';
-$DB_PORT = 3306;
+$DB_PORT = 3307;
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 
